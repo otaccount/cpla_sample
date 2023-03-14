85fa9d92fe7703ab8263649ce43f3116bfad0fac
oota <oota.123@gmail.com>
2023-03-14 22:28:13 +0900

    first regsit

---
+++ cpla_sample/cpla_sample.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int main()
+{
+    int N; // NLISTの要素数
+    int N1, N2;
+    cin >> N >> N1 >> N2;
+
+    vector<int> NLIST(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> NLIST[i];
+    }
+
+    // 出力
+    cout << "1行目:" << N << "\n";
+    cout << "2行目合計:" << N1 + N2 << "\n";
+    int sum = accumulate(NLIST.begin(), NLIST.end(), 0);
+    cout << "3行目合計:" << sum << endl;
+}
