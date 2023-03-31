57a3c5b8a4f7c855e980e279785159f1331adbce
oota <oota.123@gmail.com>
2023-03-31 23:52:49 +0900

    update file

--- cpla_sample/cpla_sample.cpp
+++ cpla_sample/cpla_sample.cpp
@@ -5,6 +5,45 @@ using namespace std;
 
 int main()
 {
+    cout << "hello world";
+}
+
+void test03() {
+    int N = 0, M = 0;
+    cin >> N >> M;
+
+    vector<int> AN(N);
+    vector<int> BM(M);
+    for (auto& i : AN) {
+        cin >> i;
+    }
+    for (auto& i : BM) {
+        cin >> i;
+    }
+
+    int ans = 0;
+    for (auto i : BM) {
+        ans += AN[i - 1];
+    }
+    cout << ans;
+}
+void test02() {
+    int N;
+    cin >> N;
+
+    vector<int> a(N);
+    for (auto& ai : a) {
+        cin >> ai;
+    }
+
+    int ans = 0;
+    for (auto i : a) {
+        ans += i;
+    }
+
+    cout << ans;
+}
+void test01() {
     int N; // NLISTの要素数
     int N1, N2;
     cin >> N >> N1 >> N2;
